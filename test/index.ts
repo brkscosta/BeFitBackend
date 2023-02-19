be967c5526d936ddd8f0c1ea5ebc4acd36455ae1
Joanã Costa <joanacosta97@hotmail.com>
2023-02-19 04:06:37 +0000

    Update packege.json and unit tests

--- test/index.ts
+++ test/index.ts
@@ -1,9 +1,10 @@
 import { expect } from 'chai';
+
 import EmailService from './../src/app/services/EmailService';
 
 describe('Email Service', async () => {
     const header = {
-        from: 'joanacosta97@gmail.com',
+        from: 'joanacosta.costa007@gmail.com',
         to: 'joanacosta97@gmail.com',
         subject: 'Teste',
     };
@@ -12,11 +13,11 @@ describe('Email Service', async () => {
     const emailMsg: EmailService = new EmailService(email);
 
     it('should create an instance using its constructor', () => {
-        expect(emailMsg, 'example should exist').to.exist; // tslint:disable-line:no-unused-expression
+        expect(emailMsg, 'should exist').to.exist; // tslint:disable-line:no-unused-expression
     });
 
-    it('should return false when the email is sent', async () => {
+    it('should return true when the email is sent', async () => {
         const returnValue = await emailMsg.sendMail();
-        expect(returnValue.valueOf).to.be.false('Because the credentials are wrong');
+        expect(returnValue).to.be.true;
     });
 });
