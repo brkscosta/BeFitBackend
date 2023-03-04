08aa080e1660e888f45dc42099fd366b842428a1
Joanã Costa <Joana.SC.Costa@ctw.bmwgroup.com>
2023-03-04 23:00:12 +0000

    Added static logger to the middlewares

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,25 +1,23 @@
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 import CErrorHandlerBase from '../errors/CErrorHandlerBase';
-import CLogger from '../utils/CLogger';
-
-const logger = new CLogger();
+import CServer from './../CServer';
 
 export const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
     if (error instanceof CErrorHandlerBase) {
         const errorMessages = error.composedErrorMessage;
 
         if (Object.keys(errorMessages).length === 0 && errorMessages.constructor === Object) {
-            logger.error(error.message);
+            CServer.getLogger().error(error.message);
             return res.status(error.statusCode).json({ error: error.message });
         }
 
-        logger.error('', { errorMessages });
+        CServer.getLogger().error(JSON.stringify(errorMessages));
         return res.status(error.statusCode).json({ error: errorMessages });
     }
 
     next(error);
-    logger.error('', { error: error.message });
+    CServer.getLogger().error(error.message);
     return res.status(500).json({ error: 'Internal Server Error' });
 };
 
@@ -27,6 +25,7 @@ export const authenticateToken = (req: Request, res: Response, next: NextFunctio
     const authHeader = req.headers.authorization;
 
     if (!authHeader) {
+        CServer.getLogger().error('No token provided');
         res.status(401).json({ message: 'No token provided' });
         return;
     }
@@ -34,6 +33,7 @@ export const authenticateToken = (req: Request, res: Response, next: NextFunctio
     const parts = authHeader.split(' ');
 
     if (parts.length <= 1) {
+        CServer.getLogger().error('Token error');
         res.status(401).json({ message: 'Token error' });
         return;
     }
@@ -41,12 +41,14 @@ export const authenticateToken = (req: Request, res: Response, next: NextFunctio
     const [scheme, token] = parts;
 
     if (!/^Bearer$/i.test(scheme)) {
+        CServer.getLogger().error('Token malformated');
         res.status(401).json({ message: 'Token malformated' });
         return;
     }
 
     verify(token, process.env.ACCESS_TOKEN_SECRET as string, (err, user) => {
         if (err) {
+            CServer.getLogger().error('Invalid token');
             res.status(401).json({ message: 'Invalid token' });
             return;
         }
